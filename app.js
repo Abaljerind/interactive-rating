feb6bc9f11cd1d72dcc95f0b06bc0a24161bcb48
Abaljerind <abaljerinda@gmail.com>
2023-05-17 20:52:02 +0700

    add script to index + create const

---
+++ app.js
@@ -0,0 +1,12 @@
+const satu = document.querySelector(".one");
+const dua = document.querySelector(".two");
+const tiga = document.querySelector(".three");
+const empat = document.querySelector(".four");
+const lima = document.querySelector(".five");
+const btn = document.querySelector("button.btn");
+
+console.log(satu.innerText, dua.innerText, tiga.innerText, empat.innerText, lima.innerText);
+
+console.dir(btn);
+
+// function button submit
