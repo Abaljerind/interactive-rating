7b8cdb807fda44b27eb701b05e5531ec104def0b
Abaljerind <abaljerinda@gmail.com>
2023-05-17 22:47:02 +0700

    add eventListener to rating

--- app.js
+++ app.js
@@ -5,8 +5,37 @@ const empat = document.querySelector(".four");
 const lima = document.querySelector(".five");
 const btn = document.querySelector("button.btn");
 
-console.log(satu.innerText, dua.innerText, tiga.innerText, empat.innerText, lima.innerText);
+const receiver = document.querySelector("span#value");
 
-console.dir(btn);
+const card = document.querySelector(".card");
 
-// function button submit
+const btnClose = document.querySelector(".btn-close");
+
+satu.addEventListener("click", function () {
+  receiver.innerText = satu.innerText;
+});
+
+dua.addEventListener("click", function () {
+  receiver.innerText = dua.innerText;
+});
+
+tiga.addEventListener("click", function () {
+  receiver.innerText = tiga.innerText;
+});
+
+empat.addEventListener("click", function () {
+  receiver.innerText = empat.innerText;
+});
+
+lima.addEventListener("click", function () {
+  receiver.innerText = lima.innerText;
+});
+
+// // function button submit
+btn.addEventListener("click", function () {
+  card.style.display = "none";
+});
+
+btnClose.addEventListener("click", function () {
+  card.style.display = "block";
+});
